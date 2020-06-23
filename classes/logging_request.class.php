a8fac2ea7fed932b98ef6dab5c03d2bc4e40b170
Grijesh Saini <gsaini@turnitin.com>
2020-06-23 16:43:49 +0530

    INT-14488 - Addressed code checker warnings.

--- classes/logging_request.class.php
+++ classes/logging_request.class.php
@@ -69,7 +69,8 @@ class plagiarism_turnitinsim_logging_request {
      * @param bool $sendsecrets The boolean value, if true send secrets as part of logs.
      */
     public function send_error_to_turnitin(plagiarism_turnitinsim_logging_request_info $loggingrequestinfo = null,
-                                           plagiarism_turnitinsim_logging_request_event_info $loggingrequesteventinfo = null, $sendsecrets = false) {
+                                           plagiarism_turnitinsim_logging_request_event_info $loggingrequesteventinfo = null,
+                                           $sendsecrets = false) {
 
         if (!get_config('plagiarism_turnitinsim', 'turnitinenableremotelogging')) {
             return;
