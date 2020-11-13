a0896c55ff3095723492783a28d47e29e73b1aa2
Jordan Marshall <jmarshall@turnitin.com>
2020-11-13 14:11:26 +0000

    Dont show link for feedback and intro files

--- lib.php
+++ lib.php
@@ -174,6 +174,16 @@ class plagiarism_plugin_turnitinsim extends plagiarism_plugin {
         }
         $output = '';
 
+        // Don't show links for certain file types as they won't have been submitted to Turnitin.
+        if (!empty($linkarray["file"])) {
+            $file = $linkarray["file"];
+            $filearea = $file->get_filearea();
+            $nonsubmittingareas = array("feedback_files", "introattachment");
+            if (in_array($filearea, $nonsubmittingareas)) {
+                return $output;
+            }
+        }
+
         // If this is a quiz, retrieve the cmid.
         $component = (!empty($linkarray['component'])) ? $linkarray['component'] : "";
         if ($component == "qtype_essay" && !empty($linkarray['area'])) {
