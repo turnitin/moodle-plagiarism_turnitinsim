a1bfa6b31e84520f46a5a5dc28e91992e2b57c93
Jordan Marshall <jmarshall@turnitin.com>
2021-02-04 13:12:37 +0000

    Change upgrade version logic

--- db/upgrade.php
+++ db/upgrade.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 /**
  * Database upgrade script for plagiarism_turnitinsim component.
  *
- * @param int $oldversion The version that is currently installed.
+ * @param int $oldversion The version that is currently installed. (The version being upgraded from)
  * @return bool true if upgrade was successful.
  * @throws ddl_exception
  * @throws ddl_table_missing_exception
@@ -307,7 +307,9 @@ function xmldb_plagiarism_turnitinsim_upgrade($oldversion) {
     /**
      * Remove "/api" from the turnitin URL as its been added to the endpoint constants.
      */
-    if ($oldversion > 2021020401) {
+    if ($oldversion < 2021020401) {
+        (new handle_deprecation)->unset_turnitinsim_use();
+
         $turnitinapiurl = get_config('plagiarism_turnitinsim', 'turnitinapiurl');
 
         set_config('turnitinapiurl', str_replace("/api", '', $turnitinapiurl), 'plagiarism_turnitinsim');
