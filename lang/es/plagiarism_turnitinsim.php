ee4c3a97b9be3484033db8920ac608eb4c986ac9
Isaac Xiong <ixiong@turnitin.com>
2023-05-18 16:39:11 -0500

    add translated map

---
+++ lang/es/plagiarism_turnitinsim.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for plagiarism_turnitinsim component, language: Mexican Spanish.
+ *
+ * This file should only contain *_link strings as other strings are handling through AMOS.
+ *
+ * @package   plagiarism_turnitinsim
+ * @copyright 2023 Turnitin
+ * @author    Isaac Xiong <ixiong@turnitin.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['help_link'] = 'https://help.turnitin.com/es/integrity/instructor/moodle.htm';
+
+$string['accessstudents'] = 'Allow students to view Similarity Reports';
+$string['accessoptions'] = 'Student access';
+$string['accessoptions_help'] = 'Students will have access to the Similarity Report for their submission after it generates.';
+$string['addtoindex'] = 'Index all submissions';
+$string['excludebiblio'] = 'Bibliography';
+$string['excludeoptions'] = 'Excluir de los informes de similitud';
+$string['excludeoptions_help'] = 'Selected options will not show as a match in Similarity Reports.';
+$string['excludequotes'] = 'Quotes';
+$string['indexoptions'] = 'Submission indexing';
+$string['turnitinmodenabled'] = 'Enable Turnitin Integrity for {$a}';
+$string['turnitinpluginenabled'] = 'Enable Turnitin';
+$string['turnitinpluginsettings'] = 'Turnitin Integrity plugin settings';
+$string['turnitinsim:enable'] = 'Enable Turnitin Integrity';
+$string['queuedrafts_help'] = 'Please note that draft submissions will not be indexed in Turnitin for checking against';
+$string['queuedrafts'] = 'Process draft submissions';
+$string['reportgen0'] = 'Immediately';
+$string['reportgen1'] = 'Immediately and regenerate on due date';
+$string['reportgen2'] = 'Due Date';
+$string['reportgenoptions'] = 'Generate Similarity Reports';
+$string['reportgenoptions_help'] = '<strong>Immediately:</strong> Similarity Reports will generate immediately after the file has been submitted.<br/><br/><strong>On due date:</strong> Similarity Reports will only generate on the due date of the assignment.<br/><br/><strong>Immediately and on due date:</strong> A Similarity Report will generate immediately after the file has been submitted. The Similarity Report will generate again on the due date of the assignment. This option can be used to check for collusion within a class.';
+$string['settingslearnmore'] = 'Learn more about Turnitin settings';
+$string['indexoptions_help'] = 'Indexed submissions will be available for comparison in Similarity Reports.';
+
