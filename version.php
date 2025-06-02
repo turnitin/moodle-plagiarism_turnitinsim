d28b1b9381bb6f4125e6975a52065540fa34ed17
Jack <jmilgate@turnitin.com>
2025-06-02 10:47:44 +0100

    Bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025052901;
+$plugin->version = 2025052902;
 $plugin->release = "v1.2";
 $plugin->requires = 2022112800;
 $plugin->component = 'plagiarism_turnitinsim';
